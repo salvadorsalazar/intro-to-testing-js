c10e05758cb73234c3f88d8213b55669692b6ec6
salvador salazar <salvador.g.salazar79@gmail.com>
2021-07-13 16:18:12 -0500

    into to testing

--- code.js
+++ code.js
@@ -2,3 +2,11 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+functin sayHello(name){
+    if (name === undefined || name === true || name === false){
+        return "hello World!";
+    }else{
+        return
+    }
+}
--- tests.js
+++ tests.js
@@ -13,3 +13,20 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+
+//unit test for sayHello function
+describe('sayHello',function(){
+    it('should be a defined function',function(){
+        expect(typeof sayHello).toBe('function');
+    });
+    it('should return a string when called', function() {
+        expect(typeof sayHello()).toBe("string");
+    });
+    it('should return the string "hello, name!" when executed', function() {
+        expect(sayHello("name")).toBe("hello, name!");
+    });
+    it("should never return 'undefined' when called", function() {
+        expect(sayHello()).not.toBe(undefined);
+    });
+});
